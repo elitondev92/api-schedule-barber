1679e520e76939982a457621a16e565911175ce2
Jonatas <jonatasserra@outlook.com>
2022-10-06 20:37:49 -0300

    feat(auth): implement guards on barbers

--- src/barbers/barbers.controller.ts
+++ src/barbers/barbers.controller.ts
@@ -1,15 +1,28 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { BarbersService } from './barbers.service';
 
 @Controller('barbers')
 export class BarbersController {
   constructor(private readonly babersService: BarbersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll(): any {
     return this.babersService.findAll();
   }
 
+  @IsPublic()
+  @UseGuards(JwtAuthGuard)
   @Post('signup')
   create(@Body() createBarberDto: any): any {
     return this.babersService.create(createBarberDto);
