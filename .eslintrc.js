4bac3e9acbb1fee63a155c5255ca7b1ac32691de
Jonatas <jonatasserra@outlook.com>
2022-10-11 22:06:56 -0300

    fix(main) mensagem error

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    "@typescript-eslint/ban-types": "error",
+    "@typescript-eslint/no-unused-vars": "off",
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,13 @@ import AppError from './errors/AppError';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((err: Error, _request: Request, response: Response) => {
+  app.use((err: Error, request: Request, response: Response, _next: any) => {
     if (err instanceof AppError) {
       return response.status(err.statusCode).json({
         status: 'error',
         message: err.message,
       });
     }
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
   });
   app.enableCors();
   await app.listen(3000);
