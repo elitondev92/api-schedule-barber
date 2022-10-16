aeb26514a399f9d0ba6c7110d3bd6bbf3fddf210
Jonatas <jonatasserra@outlook.com>
2022-10-16 20:08:25 -0300

    feat(stylist): Create with upload image

--- src/stylist/stylist.controller.ts
+++ src/stylist/stylist.controller.ts
@@ -8,11 +8,17 @@ import {
   Delete,
   Request,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { StylistService } from './stylist.service';
 import { CreateStylistDto } from './dto/create-stylist.dto';
 import { UpdateStylistDto } from './dto/update-stylist.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
+import * as crypto from 'crypto';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('stylist')
@@ -43,5 +49,40 @@ export class StylistController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.stylistService.remove(id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './tmp/uploads',
+        filename: (req, file, cb) => {
+          crypto.randomBytes(16, (err, hash) => {
+            if (err) cb(err, file.filename);
+
+            const fileName = `${hash.toString('hex')}-${extname(
+              file.originalname,
+            )}`;
+
+            cb(null, fileName);
+          });
+        },
+      }),
+      fileFilter: (req, file, cb) => {
+        if (file.mimetype.match(/\/(jpg|jpeg|png|gif)$/)) {
+          cb(null, true);
+        } else {
+          cb(new Error('Invalid file type'), false);
+        }
+      },
+    }),
+  )
+  async uploadFile(@UploadedFile() file) {
+    return (
+      this.stylistService.saveFile(file.filename),
+      {
+        url: `https://${process.env.AWS_BUCKET}.s3.amazonaws.com/${file.filename}`,
+      }
+    );
   }
 }
--- src/stylist/stylist.service.ts
+++ src/stylist/stylist.service.ts
@@ -5,13 +5,22 @@ import { CreateStylistDto } from './dto/create-stylist.dto';
 import { UpdateStylistDto } from './dto/update-stylist.dto';
 import { Stylist, StylistDocument } from './entities/stylist.entity';
 import AppError from '../errors/AppError';
+import { S3 } from 'aws-sdk';
+import { resolve } from 'path';
+import * as fs from 'fs';
 
 @Injectable()
 export class StylistService {
+  private client: S3;
+
   constructor(
     @InjectModel(Stylist.name)
     private stylistModel: Model<StylistDocument>,
-  ) {}
+  ) {
+    this.client = new S3({
+      region: 'us-east-1',
+    });
+  }
 
   public async create(createStylistDto: CreateStylistDto) {
     const checkStylist = await this.stylistModel
@@ -44,5 +53,43 @@ export class StylistService {
 
   remove(id: string) {
     return this.stylistModel.findByIdAndDelete(id).exec();
+  }
+  // upload file to aws s3 bucket
+  public async saveFile(file: string): Promise<string> {
+    const originalPath = resolve(__dirname, '..', '..', 'tmp', 'uploads', file);
+
+    const mime = require('mime');
+
+    const ContentType = mime.getType(originalPath);
+
+    if (!ContentType) {
+      throw new AppError('File not found');
+    }
+
+    const fileContent = fs.readFileSync(originalPath);
+
+    await this.client
+      .putObject({
+        Bucket: process.env.AWS_BUCKET,
+        Key: file,
+        ACL: 'public-read',
+        Body: fileContent,
+        ContentType,
+        ContentDisposition: `inline; filename=${file}`,
+      })
+      .promise();
+
+    await fs.promises.unlink(originalPath);
+
+    return file;
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    await this.client
+      .deleteObject({
+        Bucket: process.env.AWS_BUCKET,
+        Key: file,
+      })
+      .promise();
   }
 }
